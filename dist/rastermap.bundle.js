3c52e0282d9395c26d6106c688ea43f45f4d1c80
Presbyterian Librarian <jjhembd@gmail.com>
2019-06-18 10:41:49 -0500

    Return global map coordinates from selection.js (not tile pixels)

--- dist/rastermap.bundle.js
+++ dist/rastermap.bundle.js
@@ -3151,11 +3151,29 @@ function initSelector(size, boxes) {
     if (!data) return;
 
     // Compute pixel within tile
-    var ratio = box.sw / size;
-    var tileX = (mapX - ix * size) * ratio + box.sx;
-    var tileY = (mapY - iy * size) * ratio + box.sy;
+    var frac = box.sw / size;  // Fraction of the tile we will use
+    var tileX = (mapX - ix * size) * frac + box.sx;
+    var tileY = (mapY - iy * size) * frac + box.sy;
 
-    return findNearest(tileX, tileY, threshold, data.features);
+    // Scale the threshold by frac to make it a displayed distance
+    // rather than a distance in local tile coordinates
+    var feature = findNearest(tileX, tileY, threshold * frac, data.features);
+
+    if (!feature.geometry) return feature; // No actual feature selected
+
+    // TODO: The below ASSUMES feature.geometry.type === "Point"
+
+    // Make a deep copy of the feature (not just a link to the original)
+    feature = JSON.parse(JSON.stringify(feature));
+
+    // Transform the feature coordinates from tile coordinates back to 
+    //  global map coordinates
+    var coords = feature.geometry.coordinates;
+    var numTiles = 2 ** box.tile.z;
+    coords[0] = (box.tile.x + coords[0] / size) / numTiles;
+    coords[1] = (box.tile.y + coords[1] / size) / numTiles;
+
+    return feature;
   }
 
   function findNearest(x, y, threshold, features) {
